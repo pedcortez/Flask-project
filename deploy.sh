77f5fffc6e71909bc418a397d29a7debb0fa11aa
pedro cortez <42501304+pedcortez@users.noreply.github.com>
2022-01-25 17:17:28 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 # build all the images
-docker build -t pedrocortez/multi-client:latest -t pedrocortez/flask-app -f /Dockerfile
+docker build -t pedrocortez/todo-app:latest -t pedrocortez/flask-app -f /Dockerfile
 
 
 # Apply k8s config files
 
 kubectl apply -f k8s   
-kubectl set image deployments/flask-deployment client=pedrocortez/flask-app
+kubectl set image deployments/todo-app client=pedrocortez/todo-app
